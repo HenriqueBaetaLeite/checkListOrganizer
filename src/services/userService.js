a02d999f511facd4ff199ea9e9c5b6889cf49857
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2024-04-08 23:31:56 -0300

    feat: userService CRUD

--- src/services/userService.js
+++ src/services/userService.js
@@ -5,6 +5,37 @@ const getUsersService = async () => {
   return allUsers;
 };
 
+const getUserbyIdService = async (id) => {
+  const user = await User.findByPk(id);
+  return user;
+};
+
+const createUserService = async (user) => {
+  const newUser = await User.create(user);
+  return newUser;
+};
+
+const updateUserService = async (id, user) => {
+  const updatedUser = await User.update(user, {
+    where: {
+      id,
+    },
+  });
+  return updatedUser;
+};
+
+const deleteUserService = async (id) => {
+  await User.destroy({
+    where: {
+      id,
+    },
+  });
+};
+
 module.exports = {
-    getUsersService,
+  getUsersService,
+  getUserbyIdService,
+  createUserService,
+  updateUserService,
+  deleteUserService,
 };
