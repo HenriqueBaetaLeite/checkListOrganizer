fe66507a2e7980d3f32da0d89c5a2a4ed76488e1
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2024-04-10 14:21:05 -0300

    feat: completing CRUD user router

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,9 +1,21 @@
 const router = require("express").Router();
 
-const { getAllUsers, getUser } = require("../controllers/userController.js");
+const {
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController.js");
 
 router.get("/", getAllUsers);
 
 router.get("/:id", getUser);
+
+router.post("/", createUser);
+
+router.put("/:id", updateUser);
+
+router.delete("/:id", deleteUser);
 
 module.exports = router;
