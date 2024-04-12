6997a9270a527769fd0848588f9d6b3860afc382
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2024-04-12 20:16:56 -0300

    fix: some tests on integration test

--- src/tests/integration/app.test.js
+++ src/tests/integration/app.test.js
@@ -2,7 +2,7 @@ const app = require('../../app');
 const request = require('supertest');
 
 describe('Request to app', () => {
-  test('should return 200 when calling /health', async () => {
+  it('should return 200 when calling /health', async () => {
     const response = await request(app).get('/health');
     expect(response.status).toBe(200);
   });
